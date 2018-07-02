b58436a58ba373676bee145567d813712c90defb
Pratik Somaiya <pratik.somaiya2000@gmail.com>
2018-07-03 01:50:10 +0530

    fix indent

--- src/ur5_barrett_planning_node.cpp
+++ src/ur5_barrett_planning_node.cpp
@@ -45,100 +45,100 @@
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "ur5_barrett_planning_node");
-  ros::NodeHandle node_handle;  
-  ros::AsyncSpinner spinner(1);
-  spinner.start();
+	ros::init(argc, argv, "ur5_barrett_planning_node");
+	ros::NodeHandle node_handle;  
+	ros::AsyncSpinner spinner(1);
+	spinner.start();
 
 
-  /* This sleep is ONLY to allow Rviz to come up */
-  sleep(20.0);
-  
-  // BEGIN_TUTORIAL
-  // 
-  // Setup
-  // ^^^^^
-  // 
-  // The :move_group_interface:`MoveGroup` class can be easily 
-  // setup using just the name
-  // of the group you would like to control and plan for.
-  moveit::planning_interface::MoveGroup group("arm");
+	/* This sleep is ONLY to allow Rviz to come up */
+	sleep(20.0);
 
-  // We will use the :planning_scene_interface:`PlanningSceneInterface`
-  // class to deal directly with the world.
-  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;  
+	// BEGIN_TUTORIAL
+	// 
+	// Setup
+	// ^^^^^
+	// 
+	// The :move_group_interface:`MoveGroup` class can be easily 
+	// setup using just the name
+	// of the group you would like to control and plan for.
+	moveit::planning_interface::MoveGroup group("arm");
 
-  // (Optional) Create a publisher for visualizing plans in Rviz.
-  ros::Publisher display_publisher = node_handle.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true);
-  moveit_msgs::DisplayTrajectory display_trajectory;
+	// We will use the :planning_scene_interface:`PlanningSceneInterface`
+	// class to deal directly with the world.
+	moveit::planning_interface::PlanningSceneInterface planning_scene_interface;  
 
-  // Getting Basic Information
-  // ^^^^^^^^^^^^^^^^^^^^^^^^^
-  //
-  // We can print the name of the reference frame for this robot.
-  ROS_INFO("Reference frame: %s", group.getPlanningFrame().c_str());
-  
-  // We can also print the name of the end-effector link for this group.
-  ROS_INFO("Reference frame: %s", group.getEndEffectorLink().c_str());
+	// (Optional) Create a publisher for visualizing plans in Rviz.
+	ros::Publisher display_publisher = node_handle.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true);
+	moveit_msgs::DisplayTrajectory display_trajectory;
 
-  // Planning to a Pose goal
-  // ^^^^^^^^^^^^^^^^^^^^^^^
-  // We can plan a motion for this group to a desired pose for the 
-  // end-effector.
-  /*geometry_msgs::Pose target_pose1;
-  target_pose1.orientation.w = 1.0;
-  target_pose1.position.x = 1.0;
-  target_pose1.position.y = 1.0;
-  target_pose1.position.z = 1.0;
-  group.setPoseTarget(target_pose1);*/
-  
-  geometry_msgs::PoseStamped target_pose = group.getRandomPose();
-  group.setPoseTarget(target_pose); 
+	// Getting Basic Information
+	// ^^^^^^^^^^^^^^^^^^^^^^^^^
+	//
+	// We can print the name of the reference frame for this robot.
+	ROS_INFO("Reference frame: %s", group.getPlanningFrame().c_str());
+
+	// We can also print the name of the end-effector link for this group.
+	ROS_INFO("Reference frame: %s", group.getEndEffectorLink().c_str());
+
+	// Planning to a Pose goal
+	// ^^^^^^^^^^^^^^^^^^^^^^^
+	// We can plan a motion for this group to a desired pose for the 
+	// end-effector.
+	/*geometry_msgs::Pose target_pose1;
+	  target_pose1.orientation.w = 1.0;
+	  target_pose1.position.x = 1.0;
+	  target_pose1.position.y = 1.0;
+	  target_pose1.position.z = 1.0;
+	  group.setPoseTarget(target_pose1);*/
+
+	geometry_msgs::PoseStamped target_pose = group.getRandomPose();
+	group.setPoseTarget(target_pose); 
 
 
-  // Now, we call the planner to compute the plan
-  // and visualize it.
-  // Note that we are just planning, not asking move_group 
-  // to actually move the robot.
-  moveit::planning_interface::MoveGroup::Plan my_plan;
-  bool success = group.plan(my_plan);
+	// Now, we call the planner to compute the plan
+	// and visualize it.
+	// Note that we are just planning, not asking move_group 
+	// to actually move the robot.
+	moveit::planning_interface::MoveGroup::Plan my_plan;
+	bool success = group.plan(my_plan);
 
-  ROS_INFO("Visualizing plan (pose goal) %s",success?"":"FAILED");    
-  /* Sleep to give Rviz time to visualize the plan. */
-  sleep(5.0);
+	ROS_INFO("Visualizing plan (pose goal) %s",success?"":"FAILED");    
+	/* Sleep to give Rviz time to visualize the plan. */
+	sleep(5.0);
 
-  // Visualizing plans
-  // ^^^^^^^^^^^^^^^^^
-  // Now that we have a plan we can visualize it in Rviz.  This is not
-  // necessary because the group.plan() call we made above did this
-  // automatically.  But explicitly publishing plans is useful in cases that we
-  // want to visualize a previously created plan.
-  if (1)
-  {
-    ROS_INFO("Visualizing plan (again)");    
-    display_trajectory.trajectory_start = my_plan.start_state_;
-    display_trajectory.trajectory.push_back(my_plan.trajectory_);
-    display_publisher.publish(display_trajectory);
-    /* Sleep to give Rviz time to visualize the plan. */
-    sleep(5.0);
-  }
-  
-  // Moving to a pose goal
-  // ^^^^^^^^^^^^^^^^^^^^^
-  //
-  // Moving to a pose goal is similar to the step above
-  // except we now use the move() function. Note that
-  // the pose goal we had set earlier is still active 
-  // and so the robot will try to move to that goal. We will
-  // not use that function in this tutorial since it is 
-  // a blocking function and requires a controller to be active
-  // and report success on execution of a trajectory.
- 
-  /* Uncomment below line when working with a real robot*/
-  /* group.move() */
-  sleep(4.0);
+	// Visualizing plans
+	// ^^^^^^^^^^^^^^^^^
+	// Now that we have a plan we can visualize it in Rviz.  This is not
+	// necessary because the group.plan() call we made above did this
+	// automatically.  But explicitly publishing plans is useful in cases that we
+	// want to visualize a previously created plan.
+	if (1)
+	{
+		ROS_INFO("Visualizing plan (again)");    
+		display_trajectory.trajectory_start = my_plan.start_state_;
+		display_trajectory.trajectory.push_back(my_plan.trajectory_);
+		display_publisher.publish(display_trajectory);
+		/* Sleep to give Rviz time to visualize the plan. */
+		sleep(5.0);
+	}
 
-// END_TUTORIAL
-  ros::shutdown();  
-  return 0;
+	// Moving to a pose goal
+	// ^^^^^^^^^^^^^^^^^^^^^
+	//
+	// Moving to a pose goal is similar to the step above
+	// except we now use the move() function. Note that
+	// the pose goal we had set earlier is still active 
+	// and so the robot will try to move to that goal. We will
+	// not use that function in this tutorial since it is 
+	// a blocking function and requires a controller to be active
+	// and report success on execution of a trajectory.
+
+	/* Uncomment below line when working with a real robot*/
+	/* group.move() */
+	sleep(4.0);
+
+	// END_TUTORIAL
+	ros::shutdown();  
+	return 0;
 }
